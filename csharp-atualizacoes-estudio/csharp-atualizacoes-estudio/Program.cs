16005a14bcc2faa933d147a9773931b105078cc6
marcelooliveira <mclricardo@gmail.com>
2018-01-11 16:03:12 -0200

    .

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Program.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using csharp_6.V03;
+using csharp_6.V04;
 
 namespace csharp_6
 {
---
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/V04/Aluno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_6.V04
+{
+    //1. Com C# 6, podemos deixar as propriedades somente com getter e sem setter!
+    //class Aluno
+    //{
+    //    public string Nome { get;  }
+    //    public DateTime DataNascimento { get; }
+
+    //    public Aluno(string nome, DateTime dataNascimento)
+    //    {
+    //        this.Nome = nome;
+    //        this.DataNascimento = dataNascimento;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        //Nome = "Biff Tannen";
+    //        //a linha acima gera um ERRO:
+    //        //error CS0200: Property or indexer 'Aluno.Nome' cannot be assigned to --it is read only
+    //        return Nome;
+    //    }
+    //}
+
+    //2. por que tomamos o erro CS0200 acima no método ToString() mas não no construtor?
+    //porque no Construtor, não estamos atribuindo o valor à propriedade, mas sim 
+    //ao campo privado que é gerado por trás dos panos!
+    //Esse campo readonly só pode ser definido no construtor!
+    //O código acima é equivalente ao abaixo:
+    class Aluno
+    {
+        private readonly string nome;
+        public string Nome { get { return nome; } }
+        public DateTime DataNascimento { get; }
+
+        public Aluno(string nome, DateTime dataNascimento)
+        {
+            this.nome = nome;
+            this.DataNascimento = dataNascimento;
+        }
+
+        public override string ToString()
+        {
+            //Nome = "Biff Tannen";
+            //a linha acima gera um ERRO:
+            //error CS0200: Property or indexer 'Aluno.Nome' cannot be assigned to --it is read only
+            return Nome;
+        }
+    }
+}
