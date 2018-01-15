a15fc24e057f90421b43f146b8ba82974cbdb305
Alex Felipe <mclricardo@gmail.com>
2018-01-15 19:23:22 -0200

    784-csharpatualizacoes-video1.2-Propriedades Automáticas

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -11,6 +11,24 @@ namespace CSharp6.R01
         public void Main()
         {
             Console.WriteLine("1. Propriedades Automáticas Somente-Leitura");
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12));
+            Console.WriteLine(aluno.Nome);
+            Console.WriteLine(aluno.Sobrenome);
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+        {
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.DataNascimento = dataNascimento;
         }
     }
 }
