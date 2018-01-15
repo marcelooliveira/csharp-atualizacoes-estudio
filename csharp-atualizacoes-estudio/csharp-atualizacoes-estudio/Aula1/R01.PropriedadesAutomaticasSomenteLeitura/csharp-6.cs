653d67f21290513aa19ddff9e9d4ac9fd3f176a7
Alex Felipe <mclricardo@gmail.com>
2018-01-15 18:21:12 -0200

    .

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -11,38 +11,6 @@ namespace CSharp6.R01
         public void Main()
         {
             Console.WriteLine("1. Propriedades Automáticas Somente-Leitura");
-            Aluno marty = new Aluno("Marty", "McFly", new DateTime(1968, 06, 12));
-            //marty.Nome = "Biff";
-            //marty.Sobrenome = "Tannen";
-            //Error CS0272  The property or indexer 'Aluno.Nome' cannot be used in 
-            //this context because the set accessor is inaccessible   
-
-            Console.WriteLine(marty.Nome);
-            Console.WriteLine(marty.Sobrenome);
-        }
-    }
-
-    class Aluno
-    {
-        private readonly string nome;
-        public string Nome { get { return nome; } }
-        public string Sobrenome { get; }
-        public DateTime DataNascimento { get; }
-
-        public Aluno(string nome, string sobrenome, DateTime dataNascimento)
-        {
-            this.nome = nome;
-            this.Sobrenome = sobrenome;
-            this.DataNascimento = dataNascimento;
-        }
-
-        public override string ToString()
-        {
-            //Nome = "Biff";
-            //Sobrenome = "Tannen";
-            //a linha acima gera um ERRO:
-            //error CS0200: Property or indexer 'Aluno.Nome' cannot be assigned to --it is read only
-            return Nome;
         }
     }
 }
--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Program.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Program.cs
@@ -47,36 +47,36 @@ namespace CSharp6e7_roteiro
                     case 1:
                         new CSharp6.R01.Programa().Main();
                         break;
-                    case 2:
-                        new CSharp6.R02.Programa().Main();
-                        break;
-                    case 3:
-                        new CSharp6.R03.Programa().Main();
-                        break;
-                    case 4:
-                        new CSharp6.R04.Programa().Main();
-                        break;
-                    case 5:
-                        new CSharp6.R05.Programa().Main();
-                        break;
-                    case 6:
-                        new CSharp6.R06.Programa().Main();
-                        break;
-                    case 7:
-                        new CSharp6.R07.Programa().Main();
-                        break;
-                    case 8:
-                        new CSharp6.R08.Programa().Main();
-                        break;
-                    case 9:
-                        new CSharp6.R09.Programa().Main();
-                        break;
-                    case 10:
-                        new CSharp6.R10.Programa().Main();
-                        break;
-                    case 11:
-                        new CSharp6.R11.Programa().Main();
-                        break;
+                    //case 2:
+                    //    new CSharp6.R02.Programa().Main();
+                    //    break;
+                    //case 3:
+                    //    new CSharp6.R03.Programa().Main();
+                    //    break;
+                    //case 4:
+                    //    new CSharp6.R04.Programa().Main();
+                    //    break;
+                    //case 5:
+                    //    new CSharp6.R05.Programa().Main();
+                    //    break;
+                    //case 6:
+                    //    new CSharp6.R06.Programa().Main();
+                    //    break;
+                    //case 7:
+                    //    new CSharp6.R07.Programa().Main();
+                    //    break;
+                    //case 8:
+                    //    new CSharp6.R08.Programa().Main();
+                    //    break;
+                    //case 9:
+                    //    new CSharp6.R09.Programa().Main();
+                    //    break;
+                    //case 10:
+                    //    new CSharp6.R10.Programa().Main();
+                    //    break;
+                    //case 11:
+                    //    new CSharp6.R11.Programa().Main();
+                    //    break;
                     default:
                         break;
                 }
