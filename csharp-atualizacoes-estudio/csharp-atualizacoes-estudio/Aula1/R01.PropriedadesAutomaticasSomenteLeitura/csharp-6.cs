78b3205844e053b0fdf09a73bf0ee1e64f2793bc
marcelooliveira <mclricardo@gmail.com>
2018-01-15 15:14:06 -0200

    .
    //6. Ainda não atingimos a verdadeira imutabilidade! Por quê?
    //Vamos mostrar o que o compilador está fazendo por trás dos panos
    //ilustrando com campos privados:

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -24,8 +24,20 @@ namespace CSharp6.R01
 
     class Aluno
     {
-        public string Nome { get; private set; }
-        public string Sobrenome { get; private set; }
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            private set { nome = value; }
+        }
+
+        private string sobrenome;
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            private set { sobrenome = value; }
+        }
+
         public DateTime DataNascimento { get; private set; }
 
         public Aluno(string nome, string sobrenome, DateTime dataNascimento)
@@ -33,13 +45,6 @@ namespace CSharp6.R01
             this.Nome = nome;
             this.Sobrenome = sobrenome;
             this.DataNascimento = dataNascimento;
-        }
-
-        public override string ToString()
-        {
-            Nome = "Biff";
-            Sobrenome = "Tannen";
-            return Nome;
         }
     }
 }
