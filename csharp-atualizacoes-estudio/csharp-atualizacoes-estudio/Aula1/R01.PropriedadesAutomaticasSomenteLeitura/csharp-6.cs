dd237c08c9d82788aaac1a9e9afc3687e1ef0806
marcelooliveira <mclricardo@gmail.com>
2018-01-15 15:15:40 -0200

    .
    //7. Com C# 6, podemos deixar as propriedades somente com getter e sem
    setter!

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -24,27 +24,24 @@ namespace CSharp6.R01
 
     class Aluno
     {
-        private string nome;
-        public string Nome
-        {
-            get { return nome; }
-            private set { nome = value; }
-        }
-
-        private string sobrenome;
-        public string Sobrenome
-        {
-            get { return sobrenome; }
-            private set { sobrenome = value; }
-        }
-
-        public DateTime DataNascimento { get; private set; }
+        public string Nome { get; }
+        public string Sobrenome { get; }
+        public DateTime DataNascimento { get; }
 
         public Aluno(string nome, string sobrenome, DateTime dataNascimento)
         {
             this.Nome = nome;
             this.Sobrenome = sobrenome;
             this.DataNascimento = dataNascimento;
+        }
+
+        public override string ToString()
+        {
+            //Nome = "Tannen";
+            //Sobrenome = "Biff";
+            //a linha acima gera um ERRO:
+            //error CS0200: Property or indexer 'Aluno.Nome' cannot be assigned to --it is read only
+            return Nome;
         }
     }
 }
