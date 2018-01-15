9ebdc2ab49d3fec983c8c77c34fa77b99f750425
Alex Felipe <mclricardo@gmail.com>
2018-01-15 19:41:08 -0200

    784-csharpatualizacoes-video1.3-Propriedades Sem Setter.trec

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -15,20 +15,30 @@ namespace CSharp6.R01
             Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12));
             Console.WriteLine(aluno.Nome);
             Console.WriteLine(aluno.Sobrenome);
+
+            //aluno.Nome = "Biff";
+            //aluno.Sobrenome = "Tannen";
+            //Console.WriteLine(aluno.Nome);
+            //Console.WriteLine(aluno.Sobrenome);
         }
     }
 
     class Aluno
     {
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
-        public DateTime DataNascimento { get; set; }
+        readonly string nome;
+        public string Nome { get { return nome; } }
+
+        readonly string sobrenome;
+        public string Sobrenome { get { return sobrenome; } }
+
+        readonly DateTime dataNascimento;
+        public DateTime DataNascimento { get { return dataNascimento; } }
 
         public Aluno(string nome, string sobrenome, DateTime dataNascimento)
         {
-            this.Nome = nome;
-            this.Sobrenome = sobrenome;
-            this.DataNascimento = dataNascimento;
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.dataNascimento = dataNascimento;
         }
     }
 }
