d1d8034d80975b84a7aa71d852eab80b578d439e
marcelooliveira <mclricardo@gmail.com>
2018-01-15 15:25:48 -0200

    R03.MembrosComSintaxeDeExpressao
    //1. vamos adicionar uma propriedade para obter o nome completo:

---
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula1/R03.MembrosComSintaxeDeExpressao/csharp-6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6.R03
+{
+    class Programa
+    {
+        public void Main()
+        {
+            Console.WriteLine("3. Membros Com Corpo De Expressão");
+            Aluno marty = new Aluno("Marty", "McFly", new DateTime(1968, 06, 12));
+            //marty.Nome = "Biff";
+            //marty.Sobrenome = "Tannen";
+            //Error CS0272  The property or indexer 'Aluno.Nome' cannot be used in 
+            //this context because the set accessor is inaccessible   
+
+            Console.WriteLine(marty.Nome);
+            Console.WriteLine(marty.Sobrenome);
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; }
+        public string Sobrenome { get; }
+        public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
+
+        public Aluno(string nome, string sobrenome)
+        {
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+        }
+
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento) : this(nome, sobrenome)
+        {
+            this.DataNascimento = dataNascimento;
+        }
+
+        public string NomeCompleto
+        {
+            get { return string.Format("{0} {1}", Nome, Sobrenome); }
+        }
+    }
+}
