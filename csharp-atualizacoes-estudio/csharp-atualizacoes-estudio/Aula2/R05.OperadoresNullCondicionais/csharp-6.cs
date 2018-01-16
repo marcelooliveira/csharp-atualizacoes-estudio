1c658790a852d6dc6cef0e8e4b3b4534f35c7317
Alex Felipe <mclricardo@gmail.com>
2018-01-16 19:38:16 -0200

    .

--- csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula2/R05.OperadoresNullCondicionais/csharp-6.cs
+++ csharp-atualizacoes-estudio/csharp-atualizacoes-estudio/Aula2/R05.OperadoresNullCondicionais/csharp-6.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 using static System.String;
 using static System.DateTime;
-using static CSharp6.R05.Ano;
-using System.Collections.ObjectModel;
 
 namespace CSharp6.R05
 {
@@ -14,118 +13,52 @@ namespace CSharp6.R05
     {
         public void Main()
         {
-            Console.WriteLine("5. Operadores Null-Condicionais");
+            WriteLine("5. Operadores Null-Condicionais");
 
-            Aluno marty = new Aluno("Marty", "McFly", new DateTime(1968, 06, 12))
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12))
             {
                 Endereco = "9303 Lyon Drive Hill Valley CA",
                 Telefone = "555-4385"
             };
+            WriteLine(aluno.Nome);
+            WriteLine(aluno.Sobrenome);
 
-            Console.WriteLine(marty.Nome);
-            Console.WriteLine(marty.Sobrenome);
-            Console.WriteLine(marty.DadosPessoais);
-
-            Avaliacao melhorAvaliacao = GetMelhorNota(marty);
-
-            Console.WriteLine("Melhor Nota: {0}", melhorAvaliacao?.Nota);
-
-            marty.AdicionarAvaliacao(new Avaliacao(1, "Geografia", 8));
-            marty.AdicionarAvaliacao(new Avaliacao(1, "Matemática", 6));
-            marty.AdicionarAvaliacao(new Avaliacao(1, "História", 7));
-
-            melhorAvaliacao = GetMelhorNota(marty);
-
-            Console.WriteLine("Melhor Nota: {0}", melhorAvaliacao?.Nota);
+            WriteLine(aluno.NomeCompleto);
+            WriteLine("Idade: {0}", aluno.GetIdade());
+            WriteLine(aluno.DadosPessoais);
         }
-
-        private static Avaliacao GetMelhorNota(Aluno marty)
-        {
-            return marty.Avaliacoes
-                .OrderByDescending(a => a.Nota)
-                .FirstOrDefault();
-        }
-    }
-
-    public enum Ano
-    {
-        Primeiro,
-        Segundo,
-        Terceiro,
-        Quarto
     }
 
     class Aluno
     {
         public string Nome { get; }
+
         public string Sobrenome { get; }
+
         public string Endereco { get; set; }
+
         public string Telefone { get; set; }
+
+        public string DadosPessoais =>
+                 Format("{0}, {1}, {2}", NomeCompleto, Endereco, Telefone);
 
         public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
 
-        public Ano AnoNaEscola { get; set; } = Primeiro;
+        public string NomeCompleto => Nome + " " + Sobrenome;
 
-        public int PontosDeExperiencia()
-        {
-            switch (AnoNaEscola)
-            {
-                case Primeiro:
-                    return 0;
-                case Segundo:
-                    return 15;
-                case Terceiro:
-                    return 65;
-                case Quarto:
-                    return 80;
-                default:
-                    return 0;
-            }
-        }
+        public int GetIdade()
+            => (int)(((Now - DataNascimento).TotalDays) / 365.242199);
 
         public Aluno(string nome, string sobrenome)
         {
-            this.Nome = nome;
-            this.Sobrenome = sobrenome;
+            Nome = nome;
+            Sobrenome = sobrenome;
         }
-
-        public Aluno(string nome, string sobrenome, DateTime dataNascimento) : this(nome, sobrenome)
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento) :
+            this(nome, sobrenome)
         {
             this.DataNascimento = dataNascimento;
         }
 
-        public string NomeCompleto => Format("{0} {1}", Nome, Sobrenome);
-
-        public int GetIdade()
-            => (int)((Now - DataNascimento).TotalDays / 365.242199);
-
-        public string DadosPessoais =>
-            Format("Nome: {0}, " +
-                "Endereço: {1}, " +
-                "Telefone: {2}", NomeCompleto, Endereco, Telefone);
-
-
-        private IList<Avaliacao> avaliacoes = new List<Avaliacao>();
-        public IReadOnlyCollection<Avaliacao> Avaliacoes
-            => new ReadOnlyCollection<Avaliacao>(avaliacoes);
-
-        public void AdicionarAvaliacao(Avaliacao avaliacao)
-        {
-            avaliacoes.Add(avaliacao);
-        }
-    }
-
-    class Avaliacao
-    {
-        public Avaliacao(int bimestre, string materia, double nota)
-        {
-            Bimestre = bimestre;
-            Materia = materia;
-            Nota = nota;
-        }
-
-        public int Bimestre { get; }
-        public string Materia { get; }
-        public double Nota { get; }
     }
 }
